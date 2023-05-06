b5a45e766c77f20130f84232b60dee7f8cc772d2
Kelvin <ngotho.kelvin@yahoo.com>
2023-05-06 19:47:18 +0300

    Add table and Queries

--- queries.sql
+++ queries.sql
@@ -127,7 +127,64 @@ GROUP BY owners.full_name
 ORDER BY count DESC
 LIMIT 1;
 
-
+-- Who was the last animal seen by William Tatcher?
+SELECT a.name
+FROM animals a
+INNER JOIN visits v ON a.id = v.animal_id
+WHERE v.vet_id = (SELECT id FROM vets WHERE name = 'William Tatcher')
+ORDER BY v.visit_date DESC
+LIMIT 1;
+-- How many different animals did Stephanie Mendez see?
+SELECT COUNT(DISTINCT animal_id)
+FROM visits
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez');
+-- List all vets and their specialties, including vets with no specialties.
+SELECT v.name, s.name AS specialty
+FROM vets v
+LEFT JOIN specializations sp ON v.id = sp.vet_id
+LEFT JOIN species s ON sp.species_id = s.id
+ORDER BY v.name;
+-- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+SELECT a.name
+FROM animals a
+INNER JOIN visits v ON a.id = v.animal_id
+WHERE v.vet_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez')
+AND v.visit_date BETWEEN '2020-04-01' AND '2020-08-30';
+-- What animal has the most visits to vets?
+SELECT a.name, COUNT(*) AS num_visits
+FROM animals a
+INNER JOIN visits v ON a.id = v.animal_id
+GROUP BY a.id
+ORDER BY num_visits DESC
+LIMIT 1;
+-- Who was Maisy Smith's first visit?
+SELECT v.name, MIN(vi.visit_date) AS first_visit
+FROM vets v
+INNER JOIN visits vi ON v.id = vi.vet_id
+WHERE v.name = 'Maisy Smith'
+GROUP BY v.name;
+-- Details for most recent visit: animal information, vet information, and date of visit.
+SELECT a.name AS animal_name, vt.name AS vet_name, visit_date
+FROM animals a
+INNER JOIN visits v ON a.id = v.animal_id
+INNER JOIN vets vt ON v.vet_id = vt.id
+WHERE v.visit_date = (SELECT MAX(visit_date) FROM visits)
+LIMIT 1;
+-- How many visits were with a vet that did not specialize in that animal's species?
+SELECT COUNT(*)
+FROM visits v
+LEFT JOIN specializations sp ON v.vet_id = sp.vet_id AND v.animal_id = sp.species_id
+WHERE sp.vet_id IS NULL;
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+SELECT a.species_id, s.name AS species_name, COUNT(*) AS num_visits
+FROM visits v
+INNER JOIN animals a ON v.animal_id = a.id
+INNER JOIN species s ON a.species_id = s.id
+INNER JOIN vets vt ON v.vet_id = vt.id
+WHERE vt.name = 'Maisy Smith'
+GROUP BY a.species_id, species_name
+ORDER BY num_visits DESC
+LIMIT 1;
 
 
 
--- schema.sql
+++ schema.sql
@@ -22,3 +22,61 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  id SERIAL PRIMARY KEY,
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE NOT NULL
+);
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (species_id, vet_id) 
+SELECT id, (SELECT id FROM vets WHERE name = 'William Tatcher') 
+FROM species WHERE name = 'Pokemon';
+
+INSERT INTO specializations (species_id, vet_id) 
+SELECT id, (SELECT id FROM vets WHERE name = 'Stephanie Mendez') 
+FROM species WHERE name IN ('Pokemon', 'Digimon');
+
+INSERT INTO specializations (species_id, vet_id) 
+SELECT id, (SELECT id FROM vets WHERE name = 'Jack Harkness') 
+FROM species WHERE name = 'Digimon';
+
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (1, 1, '2020-05-24');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (1, 3, '2020-07-22');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (2, 4, '2021-02-02');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (3, 2, '2020-01-05');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (3, 2, '2020-03-08');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (3, 2, '2020-05-14');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (4, 3, '2021-05-04');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (5, 4, '2021-02-24');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (6, 1, '2019-12-21');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (6, 1, '2020-08-10');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (6, 2, '2021-04-07');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (7, 3, '2019-09-29');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (8, 4, '2020-10-03');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (8, 4, '2020-11-04');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (9, 2, '2019-01-24');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (9, 2, '2019-05-15');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (9, 2, '2020-02-27');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (9, 2, '2020-08-03');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (10, 3, '2020-05-24');
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES (10, 1, '2021-01-11');
